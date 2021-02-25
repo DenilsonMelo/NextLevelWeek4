ddfd20de34e0467bb2804a8b906a541ffa30402a
unknown <DenilsonMelo>
2021-02-25 10:48:02 -0300

    Estrutura de leveling

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile(){
+    const { level } = useContext(ChallengesContext);
+
     return(
         <div className={styles.profileContainer}>
             <img src="https://github.com/DenilsonMelo.png" alt="Denilson Melo"/>
@@ -8,7 +13,7 @@ export function Profile(){
                 <strong>Denilson Melo</strong>
                 <p>
                     <img src="icons/level.svg" alt=""/>
-                    Level 99
+                    Level {level}
                 </p>
             </div>
         </div>
